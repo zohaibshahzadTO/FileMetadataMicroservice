d01ef3618b80dfa3509a6b352009a01fc3b82eb8
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-05-07 16:53:17 -0400

    Mass Update
    - Created html form
    - Imported necessary file handling module (multer, cors, express, body-parser)
    - Integrated middleware
    - Implemented Post and Get requests

---
+++ app.js
@@ -0,0 +1,26 @@
+// Basic required imports for Node.js
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const upload = multer({
+    dest: 'uploads/'
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/upload', upload.single('file'), (req, res, next) => {
+
+});
+
+app.listen(4000, function() {
+    console.log("Working");
+});
