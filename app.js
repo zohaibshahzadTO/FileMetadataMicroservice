85123a6a71ea9fd9e7c723c239f5a32a2b30508f
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-05-07 17:14:42 -0400

    new app.use line
    - Able retrieve multiple files from public directory

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + "/index.html")
-});
+// app.get('/', (req, res, next) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.post('/upload', upload.single('file'), (req, res, next) => {
 
